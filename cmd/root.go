fda72a0ed104ce4b567a9203ceee74ff9e3b6d2b
chris <christophpryer@gmail.com>
2021-03-17 23:39:25 -0400

    added simple file appending

--- cmd/root.go
+++ cmd/root.go
@@ -24,35 +24,38 @@ var RootCmd = &cobra.Command{
 func Run(args []string) error {
 	fmt.Printf("Targeted files: %s", args)
 
+	files := make([]byte, len(args))
+
 	for i, filepath := range args {
 		fmt.Printf("\nLoading file (%d, %s)", i, filepath)
-		readFile(filepath)
+		file := readFile(filepath)
+
+		files = append(files, file...)
 	}
+
+	fmt.Printf("\nResult: %s", files)
 
 	return nil
 }
 
-// TODO: return []byte
-func readFile(filepath string) {
+func readFile(filepath string) []byte {
 	// TODO
 	// 1. does this open for read-only?
 	// 2. if so is there a way to get fileinfo from metadata?
 	// ^ would reduce the number of times you're loading the file.
 	file, err := os.Open(filepath)
-	check(err)
+	utils.Check(err)
 	defer file.Close()
 
 	fileinfo, err := file.Stat()
-	check(err)
+	utils.Check(err)
 
 	filesize := fileinfo.Size()
 	buffer := make([]byte, filesize)
 
 	nbytes, err := file.Read(buffer)
-	check(err)
-	fmt.Printf("\n%d bytes read from %s", nbytes, filepath)
-}
-
-func check(err error) {
 	utils.Check(err)
+	fmt.Printf("\n%d bytes read from %s", nbytes, filepath)
+
+	return buffer
 }
