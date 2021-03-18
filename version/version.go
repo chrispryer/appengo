4d9ada01433d84c7a31ac42415e66e22d526a1dd
chris <christophpryer@gmail.com>
2021-03-17 20:19:21 -0400

    added nil error return

--- version/version.go
+++ version/version.go
@@ -5,5 +5,5 @@ import "fmt"
 var Version = "0.0.1"
 
 func FullVersion() (string, error) {
-	return fmt.Sprintf("appengo version: %s", Version)
+	return fmt.Sprintf("appengo version: %s", Version), nil
 }
